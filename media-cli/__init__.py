04b87761a19092a9375d429d53dcf99bd563d138
Matt Nicholson <mnicholson@spotify.com>
2018-09-27 12:19:12 -0400

    more

--- media-cli/__init__.py
+++ media-cli/__init__.py
@@ -7,3 +7,16 @@ __description__ = 'Media Cli'
 __author__ = 'sjoeboo'
 __email__ = 'sjoeboo@sjoeboo.com'
 
+logger = logging.getLogger('media-cli')
+
+def setup_logging():
+    handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+@click.group(help='Media Cli')
+@click.pass_context
+def cli(ctx):
+    setup_logging()
+
+    ctx.obj = {}
